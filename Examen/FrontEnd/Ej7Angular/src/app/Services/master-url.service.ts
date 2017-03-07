984ac6f28aef1cb85e9287d8f0cdddfe88c1f8ac
fabriram20 <fabriram20@gmail.com>
2017-03-06 19:43:06 -0500

    seteado url cloud9

--- Examen/FrontEnd/Ej7Angular/src/app/Services/master-url.service.ts
+++ Examen/FrontEnd/Ej7Angular/src/app/Services/master-url.service.ts
@@ -6,7 +6,8 @@ export class MasterUrlService {
   private _url:string;
 
   constructor() {
-    this._url = "http://localhost:1337/";
+    //this._url = "http://localhost:1337/";
+this._url = "https://examen-twf-ramirezfabrizio-fabriram20.c9users.io";
   }
 
   get url():string{
